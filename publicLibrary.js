b7d228a07d047bfc81b2fa01eb04ccaf7234fbb0
samhoudek <samhoudek@users.noreply.github.com>
2013-10-11 00:19:48 -0600

    Create publicLibrary.js

---
+++ publicLibrary.js
@@ -0,0 +1,20 @@
+var publicLibrary = {
+  shelf1: ["Cat In The Hat", "Green Eggs and Ham"],
+  shelf2: ["The Lorax", "All The Places You'll Go"],
+  shelf3: ["Horton Hears a Who", "One Fish Two Fish"]};
+ 
+ publicLibrary = [library];
+ 
+var enshelf = function(book,shelf){
+	shelf.push(book);count++;
+	count: 0;
+	}
+
+var unshelf = function(book,shelf){
+	shelf.push(book);count -1;
+	count:0};(end)
+
+	
+var countBooktotal = function() {
+	  return library.count;
+}
