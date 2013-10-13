e4e39b5a1544b9d630ab9f920eeaab570395cc36
samhoudek <samhoudek@users.noreply.github.com>
2013-10-12 21:30:41 -0600

    Update publicLibrary.js

--- publicLibrary.js
+++ publicLibrary.js
@@ -1,19 +1,35 @@
-var publicLibrary = {
-  shelf1: ["Cat In The Hat", "Green Eggs and Ham"],
-  shelf2: ["The Lorax", "All The Places You'll Go"],
-  shelf3: ["Horton Hears a Who", "One Fish Two Fish"]};
- 
- publicLibrary = [library];
- 
+function publicLibrary(){}
+
+function book (title, shelf, enshelf, unshelf){
+	this.title= title;
+	this.shelf=shelfNumber;
+	this.enshelf=enshelf;
+	this.unshelf=unshelf;}
+
+var book1 = new book ("Cat in the Hat", "1", true, false);
+var book2 = new book ("Green Eggs and Ham","2", true, false);
+
+var totalBook = [book1, book2]; 
+
+var publicLibraryInventory = function(){
+	for (i=0, i<totalBook.length;i++)
+
+function shelf (shelfNumber){
+	this.shelfNumber = shelfNumber;}
+
+var shelf1 = new shelf (1);
+var shelf2 = new shelf (2);
+
+function totalShelf = [shelf1,shelf2];
+
+
 var enshelf = function(book,shelf){
 	shelf.push(book);count++;
-	count: 0};
+	count:0}
 	
 
 var unshelf = function(book,shelf){
-	shelf.push(book);count -1;
-	count:0};
+	shelf.push(book);count --;
+	count:0}
 
 	
-var countBooktotal = function() {
-	  return library.count};
