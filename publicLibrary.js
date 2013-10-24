a8bfe40c31b542d45defb97451e2aa9d57e87388
samhoudek <samhoudek@users.noreply.github.com>
2013-10-24 13:36:26 -0600

    Update publicLibrary.js

--- publicLibrary.js
+++ publicLibrary.js
@@ -1,29 +1,25 @@
 function publicLibrary(){}
 
-function book (title, shelf, enshelf, unshelf){
+function book (title, shelf){
         this.title= title;
         this.shelf=shelfNumber;
-        this.enshelf= function(shelf){
-		shelf.book.push(this);}
-		this.unshelf=function(shelf){
-		shelf.book.pop(this);
-  }
+          }
 		
 function shelf (shelfNumber){
 		this.shelfNumber = shelfNumber;
-		this.containsBook = function (book){this.book.indexOf(book) !=-1){return true;
+		if (this.containsBook = function (book){this.book.indexOf(book) !=-1){return true;
 }
-		else { return false}}}
+		else { return false;}}}
 
 function Library (){
 this.shelves=[];
 this.addShelf = function (shelf){
 this.shelves.append(shelf);};
-this.totalBook = books.length
+this.totalBook = books.length;
 
 var bookInventory = 
-var book1 = new book ("Cat in the Hat", "shelf1", true, false);
-var book2 = new book ("Green Eggs and Ham","shelf2", true, false);
+var book1 = new book ("Cat in the Hat", "shelf1");
+var book2 = new book ("Green Eggs and Ham","shelf2");
 
 var books = [book1, book2]; 
 
@@ -35,12 +31,12 @@ var publicLibraryInventory = function(){
 
 var enshelf = function(book,shelf){
         shelf.push(book);count++;
-        count:0}
+        count:0};
         
 
 var unshelf = function(book,shelf){
         shelf.push(book);count --;
-        count:0}
+        count:0};
 		
 var publicLibrary = new Library ();
         publicLibrary.addShelf(shelf1, shelf2);
